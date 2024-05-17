c4d41d3bd7f2db2be985a7e10ea50ebc2bd1f859
Franklin M <franklinmuriithi431@gmail.com>
2024-05-17 13:27:56 +0300

    Change folder name

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav>
-            <img src="images/react-icon-small.png" className="nav--icon" />
+            <img src="img/react-icon-small.png" className="nav--icon" />
             <h3 className="nav--logo_text">React Facts</h3>
             <h4 className="nav--title">Intro to React</h4>
         </nav>
